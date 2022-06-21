82ebb2f20157b1f525d1880e8bfafa2ecf3b844d
Md Rubel Rana <96787688+rubelrana123@users.noreply.github.com>
2022-06-21 23:30:17 +0600

    add some

---
+++ script.js
@@ -0,0 +1,12 @@
+function deleteMe(){
+  document.getElementById("myResult").value = ""
+
+};
+
+function calculator(newValue) {
+  document.getElementById("myResult").value += newValue;//newvalue = newvalue +"1"
+
+};
+ function answer() {
+
+ };
